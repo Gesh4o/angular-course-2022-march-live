8c4c024cb266bd85ed9641660b3a6fecf8267b26
Georgi Stoimenov <georgi.stoimenov_96@abv.bg>
2022-03-30 17:46:17 +0300

    [Day 9] Preparation for lecture 1.0.

--- 9 - pipes jwt lazy load subjects/su-modules-and-routing/src/app/pages/home/home.component.ts
+++ 9 - pipes jwt lazy load subjects/su-modules-and-routing/src/app/pages/home/home.component.ts
@@ -8,10 +8,57 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
+  private activeUsers!: Promise<number>;
+
   constructor(private titleService: Title) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.titleService.setTitle('Home Page');
-  }
 
+    // const xhr = new XMLHttpRequest();
+    // xhr.addEventListener('load', function () {
+    //   const xhr1 = new XMLHttpRequest();
+    //   xhr1.addEventListener('load', function () {
+    //     const xhr3 = new XMLHttpRequest();
+    //     xhr3.addEventListener('load', function () {
+    //     });
+    //   });
+    // });
+
+    // const r1 = await new Promise((resolve, reject) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.addEventListener('load', function () {
+    //     resolve(this.responseText);
+    //   })
+    // });
+
+    // const r2 = await new Promise((resolve, reject) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.addEventListener('load', function () {
+    //     resolve(this.responseText);
+    //   })
+    // })
+    // const r3 = await new Promise((resolve, reject) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.addEventListener('load', function () {
+    //     resolve(this.responseText);
+    //   })
+    // })
+
+    this.activeUsers = new Promise<number>((resolve) => {
+      // setInterval(() => {
+        setTimeout(() => {
+          const activeUsers = Math.round(Math.random() * 100);
+
+          console.log('resolve', activeUsers);
+          resolve(activeUsers);
+        }, 2000)
+      // }, 4000);
+    });
+
+    this.activeUsers
+      .then(activeUsers => {
+        console.log('activeUsers', activeUsers);
+      });
+  }
 }
